36fd3cc941c7650e7810c41e4fa098a659472be2
Lander Brandt <landerbrandt@gmail.com>
2023-12-17 02:34:03 -0800

    begin translating GameParams to Params

--- src/game_params.rs
+++ src/game_params.rs
@@ -2,149 +2,29 @@ use std::{
     collections::{BTreeMap, HashMap},
     io::Cursor,
     path::Path,
+    str::FromStr,
     time::Instant,
 };
 
-use bson::{Decimal128, Document};
 use egui::ahash::HashMapExt;
 use flate2::read::ZlibDecoder;
 use itertools::Itertools;
 use ouroboros::self_referencing;
 use serde_pickle::{DeOptions, HashableValue, Value};
+use wows_replays::{
+    game_params::{Param, ParamBuilder, ParamData, Species},
+    resource_loader::{EntityType, Vehicle, VehicleBuilder},
+};
 use wowsunpack::{idx::FileNode, pkg::PkgFileLoader};
 
 use crate::error::DataLoadError;
 
-#[self_referencing]
 pub struct GameParams {
-    params: serde_pickle::Value,
+    params: wows_replays::game_params::GameParams,
     param_id_to_translation_id: HashMap<i64, String>,
     #[borrows(params)]
     #[covariant]
     param_id_to_dict: HashMap<i64, &'this BTreeMap<HashableValue, Value>>,
-}
-
-fn hashable_pickle_to_bson(pickled: &serde_pickle::HashableValue) -> bson::Bson {
-    match pickled {
-        serde_pickle::HashableValue::None => bson::Bson::Null,
-        serde_pickle::HashableValue::Bool(v) => bson::Bson::Boolean(*v),
-        serde_pickle::HashableValue::I64(v) => bson::Bson::Int64(*v),
-        serde_pickle::HashableValue::Int(_v) => todo!("Hashable int -> JSON"),
-        serde_pickle::HashableValue::F64(v) => bson::to_bson(&v).unwrap(),
-        serde_pickle::HashableValue::Bytes(v) => bson::to_bson(&v).unwrap(),
-        serde_pickle::HashableValue::String(v) => bson::Bson::String(v.clone()),
-        serde_pickle::HashableValue::Tuple(v) => {
-            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
-        }
-        serde_pickle::HashableValue::FrozenSet(v) => {
-            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
-        }
-    }
-}
-
-pub fn pickle_to_bson(pickled: &serde_pickle::Value) -> bson::Bson {
-    match pickled {
-        serde_pickle::Value::None => bson::Bson::Null,
-        serde_pickle::Value::Bool(v) => bson::Bson::Boolean(*v),
-        serde_pickle::Value::I64(v) => bson::Bson::Int64(*v),
-        serde_pickle::Value::Int(_v) => todo!("Int -> JSON"),
-        serde_pickle::Value::F64(v) => bson::to_bson(&v).unwrap(),
-        serde_pickle::Value::Bytes(v) => bson::to_bson(&v).unwrap(),
-        serde_pickle::Value::String(v) => bson::to_bson(&v).unwrap(),
-        serde_pickle::Value::List(v) => {
-            bson::Bson::Array(v.into_iter().map(pickle_to_bson).collect())
-        }
-        serde_pickle::Value::Tuple(v) => {
-            bson::Bson::Array(v.into_iter().map(pickle_to_bson).collect())
-        }
-        serde_pickle::Value::Set(v) => {
-            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
-        }
-        serde_pickle::Value::FrozenSet(v) => {
-            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
-        }
-        serde_pickle::Value::Dict(v) => {
-            let mut doc = bson::Document::new();
-            for (key, value) in v {
-                let converted_key = hashable_pickle_to_bson(key);
-                let string_key = match converted_key {
-                    bson::Bson::Int32(num) => num.to_string(),
-                    bson::Bson::Int64(num) => num.to_string(),
-                    bson::Bson::String(s) => s.to_string(),
-                    _other => {
-                        continue;
-                        // panic!(
-                        //     "Unsupported key type: {:?} (original: {:#?}, {:#?})",
-                        //     other, key, v
-                        // );
-                    }
-                };
-
-                let converted_value = pickle_to_bson(value);
-
-                doc.insert(string_key, converted_value);
-            }
-
-            bson::Bson::Document(doc)
-        }
-    }
-}
-
-fn hashable_pickle_to_cbor(pickled: &serde_pickle::HashableValue) -> cbor::Cbor {
-    match pickled {
-        serde_pickle::HashableValue::None => cbor::Cbor::Null,
-        serde_pickle::HashableValue::Bool(v) => cbor::Cbor::Bool(*v),
-        serde_pickle::HashableValue::I64(v) => cbor::Cbor::Signed(cbor::CborSigned::Int64(*v)),
-        serde_pickle::HashableValue::Int(_v) => todo!("Hashable int -> JSON"),
-        serde_pickle::HashableValue::F64(v) => cbor::Cbor::Float(cbor::CborFloat::Float64(*v)),
-        serde_pickle::HashableValue::Bytes(v) => cbor::Cbor::Bytes(cbor::CborBytes(v.clone())),
-        serde_pickle::HashableValue::String(v) => cbor::Cbor::Unicode(v.clone()),
-        serde_pickle::HashableValue::Tuple(v) => {
-            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
-        }
-        serde_pickle::HashableValue::FrozenSet(v) => {
-            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
-        }
-    }
-}
-
-pub fn pickle_to_cbor(pickled: &serde_pickle::Value) -> cbor::Cbor {
-    match pickled {
-        serde_pickle::Value::None => cbor::Cbor::Null,
-        serde_pickle::Value::Bool(v) => cbor::Cbor::Bool(*v),
-        serde_pickle::Value::I64(v) => cbor::Cbor::Signed(cbor::CborSigned::Int64(*v)),
-        serde_pickle::Value::Int(_v) => todo!("Int -> JSON"),
-        serde_pickle::Value::F64(v) => cbor::Cbor::Float(cbor::CborFloat::Float64(*v)),
-        serde_pickle::Value::Bytes(v) => cbor::Cbor::Bytes(cbor::CborBytes(v.clone())),
-        serde_pickle::Value::String(v) => cbor::Cbor::Unicode(v.clone()),
-        serde_pickle::Value::List(v) => {
-            cbor::Cbor::Array(v.into_iter().map(pickle_to_cbor).collect())
-        }
-        serde_pickle::Value::Tuple(v) => {
-            cbor::Cbor::Array(v.into_iter().map(pickle_to_cbor).collect())
-        }
-        serde_pickle::Value::Set(v) => {
-            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
-        }
-        serde_pickle::Value::FrozenSet(v) => {
-            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
-        }
-        serde_pickle::Value::Dict(v) => {
-            cbor::Cbor::Map(HashMap::from_iter(v.iter().filter_map(|(key, value)| {
-                let converted_key = hashable_pickle_to_cbor(key);
-                let string_key = match converted_key {
-                    cbor::Cbor::Unsigned(unsigned) => unsigned.into_u64().to_string(),
-                    cbor::Cbor::Signed(signed) => signed.into_i64().to_string(),
-                    cbor::Cbor::Unicode(s) => s.to_string(),
-                    other => {
-                        return None;
-                    }
-                };
-                let converted_value = pickle_to_cbor(value);
-                Some((string_key, converted_value))
-            })))
-        }
-    }
 }
 
 impl GameParams {
@@ -157,13 +37,16 @@ impl GameParams {
         println!("deserializing gameparams");
 
         let start = Instant::now();
-        let mut params: Value = if cache_path.exists() {
-            let data = std::fs::read(cache_path).unwrap();
-            let mut decoder = cbor::Decoder::from_reader(data.as_slice());
-            let des: cbor::Cbor = decoder.items().next().unwrap().unwrap();
+        let params = cache_path
+            .exists()
+            .then(|| {
+                let cache_data = std::fs::File::open(cache_path).ok()?;
+                bincode::deserialize_from(cache_data).ok()
+            })
+            .flatten();
 
-            let now = Instant::now();
-            panic!("took {} seconds to load", (now - start).as_secs());
+        let params = if let Some(params) = params {
+            params
         } else {
             let game_params = file_tree.find("content/GameParams.data")?;
             let mut game_params_data = Vec::new();
@@ -174,7 +57,7 @@ impl GameParams {
             let mut decoder = ZlibDecoder::new(Cursor::new(game_params_data));
             std::io::copy(&mut decoder, &mut decompressed_data)?;
             decompressed_data.set_position(0);
-            let params = serde_pickle::from_reader(
+            let pickled_params = serde_pickle::from_reader(
                 &mut decompressed_data,
                 DeOptions::default()
                     .replace_unresolved_globals()
@@ -182,76 +65,151 @@ impl GameParams {
             )
             .expect("failed to load game params");
 
-            let mut file = std::fs::File::create(cache_path).unwrap();
-            let mut encoder = cbor::Encoder::from_writer(&mut file);
-            encoder.encode([&pickle_to_cbor(&params)]).unwrap();
+            let new_params = if let Value::List(mut params_list) = pickled_params {
+                let params = params_list.remove(0);
+                if let Value::Dict(params_dict) = params {
+                    params_dict.values().filter_map(|param| {
+                        if let Value::Dict(param_data) = param {
+                            param_data.get(&HashableValue::String("typeinfo".to_string())).and_then(|type_info| {
+                                if let Value::Dict(type_info) = type_info {
+                                    Some((type_info.get(&HashableValue::String("nation".to_string()))?, type_info.get(&HashableValue::String("species".to_string()))?, type_info.get(&HashableValue::String("type".to_string()))?))
+                                } else {
+                                    None
+                                }
+                            }).and_then(|(nation, species, typ)| {
+                                if let (Value::String(nation), Value::String(typ)) = (nation, typ) {
+                                    let entity_type = EntityType::from_str(&typ).ok()?;
+                                    let nation = nation.clone();
+                                    let species = if let Value::String(species) = species{
+                                        Species::from_str(species).ok()
+                                    } else {
+                                        None
+                                    };
 
-            params
+                                    let parsed_param_data = match entity_type {
+                                        EntityType::Ship => {
+                                            let level = if let Value::I64(level) = param_data.get(&HashableValue::String("level".to_string())).expect("vehicle does not have level attribute") {
+                                                *level as u32
+                                            } else {
+                                                panic!("vehicle level is not an i64");
+                                            };
+                                            let group = if let Value::String(group) = param_data.get(&HashableValue::String("group".to_string())).expect("vehicle does not have group attribute") {
+                                                group.clone()
+                                            } else {
+                                                panic!("vehicle leve is not an i64");
+                                            };
+                                            VehicleBuilder::default().level(level).group(group).build().ok().map(|v| ParamData::Vehicle(v))
+                                    },
+                                    _ => None,
+                                    }?;
+
+                                    let id = if let Value::I64(id) = param_data.get(&HashableValue::String("id".to_string())).expect("param has no id field") {
+                                        *id as u32
+                                    } else {
+                                        panic!("param id is not an i64");
+                                    };
+
+                                    let index = if let Value::String(index) = param_data.get(&HashableValue::String("index".to_string())).expect("param has no index field") {
+                                        index.clone()
+                                    } else {
+                                        panic!("param index is not a string");
+                                    };
+
+                                    let name = if let Value::String(name) = param_data.get(&HashableValue::String("name".to_string())).expect("param has no name field") {
+                                        name.clone()
+                                    } else {
+                                        panic!("param name is not a string");
+                                    };
+
+                                    ParamBuilder::default().id(id).index(index).name(name).species(species).nation(nation).data(parsed_param_data).build().ok()
+                                } else {
+                                    None
+                                }
+                            })
+                        } else {
+                            None
+                        }
+                    }).collect::<Vec<wows_replays::game_params::Param>>()
+                } else {
+                    panic!("params is not a list");
+                }
+            } else {
+                panic!("game params are not a list");
+            };
+
+            let file = std::fs::File::create(cache_path).unwrap();
+            bincode::serialize_into(file, &new_params)
+                .expect("failed to serialize cached game params");
+
+            new_params
         };
 
         let now = Instant::now();
         println!("took {} seconds to load", (now - start).as_secs());
 
-        println!("loading ships");
+        panic!("{:#?}", params);
 
-        let mut ship_id_to_translation_id = HashMap::new();
-        // let mut ship_id_to_ship = HashMap::new();
-        if let Value::List(mut list) = params {
-            params = list.remove(0);
-        }
-        if let Value::Dict(dict) = &params {
-            for (key, value) in dict {
-                if let Value::Dict(ship_dict) = value {
-                    let id = ship_dict
-                        .get(&HashableValue::String("id".to_string()))
-                        .unwrap();
-                    let id = if let Value::I64(id) = id {
-                        *id
-                    } else {
-                        panic!("ID is not an i64")
-                    };
+        // println!("loading ships");
 
-                    let index = ship_dict
-                        .get(&HashableValue::String("index".to_string()))
-                        .unwrap();
-                    let index = if let Value::String(index) = index {
-                        index.clone()
-                    } else {
-                        panic!("ID is not an i64")
-                    };
+        // let mut ship_id_to_translation_id = HashMap::new();
+        // // let mut ship_id_to_ship = HashMap::new();
+        // if let Value::List(mut list) = params {
+        //     params = list.remove(0);
+        // }
+        // if let Value::Dict(dict) = &params {
+        //     for (key, value) in dict {
+        //         if let Value::Dict(ship_dict) = value {
+        //             let id = ship_dict
+        //                 .get(&HashableValue::String("id".to_string()))
+        //                 .unwrap();
+        //             let id = if let Value::I64(id) = id {
+        //                 *id
+        //             } else {
+        //                 panic!("ID is not an i64")
+        //             };
 
-                    ship_id_to_translation_id.insert(id, format!("IDS_{index}"));
-                    // ship_id_to_ship.insert(id, dict);
-                }
-            }
-        }
+        //             let index = ship_dict
+        //                 .get(&HashableValue::String("index".to_string()))
+        //                 .unwrap();
+        //             let index = if let Value::String(index) = index {
+        //                 index.clone()
+        //             } else {
+        //                 panic!("ID is not an i64")
+        //             };
 
-        Ok(GameParamsBuilder {
-            params,
-            param_id_to_translation_id: ship_id_to_translation_id,
-            param_id_to_dict_builder: |params| {
-                let mut param_id_to_dict = HashMap::new();
-                if let Value::Dict(root_dict) = &params {
-                    for (key, value) in root_dict {
-                        if let Value::Dict(ship_dict) = value {
-                            let id = ship_dict
-                                .get(&HashableValue::String("id".to_string()))
-                                .unwrap();
-                            let id = if let Value::I64(id) = id {
-                                *id
-                            } else {
-                                panic!("ID is not an i64")
-                            };
+        //             ship_id_to_translation_id.insert(id, format!("IDS_{index}"));
+        //             // ship_id_to_ship.insert(id, dict);
+        //         }
+        //     }
+        // }
+        // panic!("???");
 
-                            param_id_to_dict.insert(id, ship_dict);
-                        }
-                    }
-                }
+        // Ok(GameParamsBuilder {
+        //     params,
+        //     param_id_to_translation_id: ship_id_to_translation_id,
+        //     param_id_to_dict_builder: |params| {
+        //         let mut param_id_to_dict = HashMap::new();
+        //         if let Value::Dict(root_dict) = &params {
+        //             for (key, value) in root_dict {
+        //                 if let Value::Dict(ship_dict) = value {
+        //                     let id = ship_dict
+        //                         .get(&HashableValue::String("id".to_string()))
+        //                         .unwrap();
+        //                     let id = if let Value::I64(id) = id {
+        //                         *id
+        //                     } else {
+        //                         panic!("ID is not an i64")
+        //                     };
 
-                param_id_to_dict
-            },
-        }
-        .build())
+        //                     param_id_to_dict.insert(id, ship_dict);
+        //                 }
+        //             }
+        //         }
+
+        //         param_id_to_dict
+        //     },
+        // }
+        // .build())
     }
 
     pub fn ship_id_to_localization_id(&self, ship_id: i64) -> Option<&str> {
