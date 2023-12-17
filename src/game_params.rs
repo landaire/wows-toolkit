9f6709bcf50549f8ccf0d8b442f36ca1243b2d1b
Lander Brandt <landerbrandt@gmail.com>
2023-12-17 00:38:06 -0800

    add game params cache

--- src/game_params.rs
+++ src/game_params.rs
@@ -1,9 +1,11 @@
 use std::{
     collections::{BTreeMap, HashMap},
     io::Cursor,
+    path::Path,
     time::Instant,
 };
 
+use bson::{Decimal128, Document};
 use egui::ahash::HashMapExt;
 use flate2::read::ZlibDecoder;
 use itertools::Itertools;
@@ -22,32 +24,170 @@ pub struct GameParams {
     param_id_to_dict: HashMap<i64, &'this BTreeMap<HashableValue, Value>>,
 }
 
+fn hashable_pickle_to_bson(pickled: &serde_pickle::HashableValue) -> bson::Bson {
+    match pickled {
+        serde_pickle::HashableValue::None => bson::Bson::Null,
+        serde_pickle::HashableValue::Bool(v) => bson::Bson::Boolean(*v),
+        serde_pickle::HashableValue::I64(v) => bson::Bson::Int64(*v),
+        serde_pickle::HashableValue::Int(_v) => todo!("Hashable int -> JSON"),
+        serde_pickle::HashableValue::F64(v) => bson::to_bson(&v).unwrap(),
+        serde_pickle::HashableValue::Bytes(v) => bson::to_bson(&v).unwrap(),
+        serde_pickle::HashableValue::String(v) => bson::Bson::String(v.clone()),
+        serde_pickle::HashableValue::Tuple(v) => {
+            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
+        }
+        serde_pickle::HashableValue::FrozenSet(v) => {
+            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
+        }
+    }
+}
+
+pub fn pickle_to_bson(pickled: &serde_pickle::Value) -> bson::Bson {
+    match pickled {
+        serde_pickle::Value::None => bson::Bson::Null,
+        serde_pickle::Value::Bool(v) => bson::Bson::Boolean(*v),
+        serde_pickle::Value::I64(v) => bson::Bson::Int64(*v),
+        serde_pickle::Value::Int(_v) => todo!("Int -> JSON"),
+        serde_pickle::Value::F64(v) => bson::to_bson(&v).unwrap(),
+        serde_pickle::Value::Bytes(v) => bson::to_bson(&v).unwrap(),
+        serde_pickle::Value::String(v) => bson::to_bson(&v).unwrap(),
+        serde_pickle::Value::List(v) => {
+            bson::Bson::Array(v.into_iter().map(pickle_to_bson).collect())
+        }
+        serde_pickle::Value::Tuple(v) => {
+            bson::Bson::Array(v.into_iter().map(pickle_to_bson).collect())
+        }
+        serde_pickle::Value::Set(v) => {
+            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
+        }
+        serde_pickle::Value::FrozenSet(v) => {
+            bson::Bson::Array(v.into_iter().map(hashable_pickle_to_bson).collect())
+        }
+        serde_pickle::Value::Dict(v) => {
+            let mut doc = bson::Document::new();
+            for (key, value) in v {
+                let converted_key = hashable_pickle_to_bson(key);
+                let string_key = match converted_key {
+                    bson::Bson::Int32(num) => num.to_string(),
+                    bson::Bson::Int64(num) => num.to_string(),
+                    bson::Bson::String(s) => s.to_string(),
+                    _other => {
+                        continue;
+                        // panic!(
+                        //     "Unsupported key type: {:?} (original: {:#?}, {:#?})",
+                        //     other, key, v
+                        // );
+                    }
+                };
+
+                let converted_value = pickle_to_bson(value);
+
+                doc.insert(string_key, converted_value);
+            }
+
+            bson::Bson::Document(doc)
+        }
+    }
+}
+
+fn hashable_pickle_to_cbor(pickled: &serde_pickle::HashableValue) -> cbor::Cbor {
+    match pickled {
+        serde_pickle::HashableValue::None => cbor::Cbor::Null,
+        serde_pickle::HashableValue::Bool(v) => cbor::Cbor::Bool(*v),
+        serde_pickle::HashableValue::I64(v) => cbor::Cbor::Signed(cbor::CborSigned::Int64(*v)),
+        serde_pickle::HashableValue::Int(_v) => todo!("Hashable int -> JSON"),
+        serde_pickle::HashableValue::F64(v) => cbor::Cbor::Float(cbor::CborFloat::Float64(*v)),
+        serde_pickle::HashableValue::Bytes(v) => cbor::Cbor::Bytes(cbor::CborBytes(v.clone())),
+        serde_pickle::HashableValue::String(v) => cbor::Cbor::Unicode(v.clone()),
+        serde_pickle::HashableValue::Tuple(v) => {
+            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
+        }
+        serde_pickle::HashableValue::FrozenSet(v) => {
+            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
+        }
+    }
+}
+
+pub fn pickle_to_cbor(pickled: &serde_pickle::Value) -> cbor::Cbor {
+    match pickled {
+        serde_pickle::Value::None => cbor::Cbor::Null,
+        serde_pickle::Value::Bool(v) => cbor::Cbor::Bool(*v),
+        serde_pickle::Value::I64(v) => cbor::Cbor::Signed(cbor::CborSigned::Int64(*v)),
+        serde_pickle::Value::Int(_v) => todo!("Int -> JSON"),
+        serde_pickle::Value::F64(v) => cbor::Cbor::Float(cbor::CborFloat::Float64(*v)),
+        serde_pickle::Value::Bytes(v) => cbor::Cbor::Bytes(cbor::CborBytes(v.clone())),
+        serde_pickle::Value::String(v) => cbor::Cbor::Unicode(v.clone()),
+        serde_pickle::Value::List(v) => {
+            cbor::Cbor::Array(v.into_iter().map(pickle_to_cbor).collect())
+        }
+        serde_pickle::Value::Tuple(v) => {
+            cbor::Cbor::Array(v.into_iter().map(pickle_to_cbor).collect())
+        }
+        serde_pickle::Value::Set(v) => {
+            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
+        }
+        serde_pickle::Value::FrozenSet(v) => {
+            cbor::Cbor::Array(v.into_iter().map(hashable_pickle_to_cbor).collect())
+        }
+        serde_pickle::Value::Dict(v) => {
+            cbor::Cbor::Map(HashMap::from_iter(v.iter().filter_map(|(key, value)| {
+                let converted_key = hashable_pickle_to_cbor(key);
+                let string_key = match converted_key {
+                    cbor::Cbor::Unsigned(unsigned) => unsigned.into_u64().to_string(),
+                    cbor::Cbor::Signed(signed) => signed.into_i64().to_string(),
+                    cbor::Cbor::Unicode(s) => s.to_string(),
+                    other => {
+                        return None;
+                    }
+                };
+                let converted_value = pickle_to_cbor(value);
+                Some((string_key, converted_value))
+            })))
+        }
+    }
+}
+
 impl GameParams {
     pub fn from_pkg(
         file_tree: &FileNode,
         pkg_loader: &PkgFileLoader,
     ) -> Result<GameParams, DataLoadError> {
         println!("loading game params");
-        let game_params = file_tree.find("content/GameParams.data")?;
-        let mut game_params_data = Vec::new();
-        game_params.read_file(pkg_loader, &mut game_params_data)?;
-        game_params_data.reverse();
-
-        let mut decompressed_data = Cursor::new(Vec::new());
-        let mut decoder = ZlibDecoder::new(Cursor::new(game_params_data));
-        std::io::copy(&mut decoder, &mut decompressed_data)?;
-        decompressed_data.set_position(0);
-
+        let cache_path = Path::new("game_params.bin");
         println!("deserializing gameparams");
 
         let start = Instant::now();
-        let mut params: Value = serde_pickle::from_reader(
-            &mut decompressed_data,
-            DeOptions::default()
-                .replace_unresolved_globals()
-                .decode_strings(),
-        )
-        .expect("failed to load game params");
+        let mut params: Value = if cache_path.exists() {
+            let data = std::fs::read(cache_path).unwrap();
+            let mut decoder = cbor::Decoder::from_reader(data.as_slice());
+            let des: cbor::Cbor = decoder.items().next().unwrap().unwrap();
+
+            let now = Instant::now();
+            panic!("took {} seconds to load", (now - start).as_secs());
+        } else {
+            let game_params = file_tree.find("content/GameParams.data")?;
+            let mut game_params_data = Vec::new();
+            game_params.read_file(pkg_loader, &mut game_params_data)?;
+            game_params_data.reverse();
+
+            let mut decompressed_data = Cursor::new(Vec::new());
+            let mut decoder = ZlibDecoder::new(Cursor::new(game_params_data));
+            std::io::copy(&mut decoder, &mut decompressed_data)?;
+            decompressed_data.set_position(0);
+            let params = serde_pickle::from_reader(
+                &mut decompressed_data,
+                DeOptions::default()
+                    .replace_unresolved_globals()
+                    .decode_strings(),
+            )
+            .expect("failed to load game params");
+
+            let mut file = std::fs::File::create(cache_path).unwrap();
+            let mut encoder = cbor::Encoder::from_writer(&mut file);
+            encoder.encode([&pickle_to_cbor(&params)]).unwrap();
+
+            params
+        };
 
         let now = Instant::now();
         println!("took {} seconds to load", (now - start).as_secs());
