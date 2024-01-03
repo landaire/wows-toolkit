b629e7bc9ca4fb6582799c52481a89e597237fe2
Lander Brandt <landerbrandt@gmail.com>
2024-01-02 22:16:16 -0800

    screw it, only supporting windows builds right now

--- prebuild.sh
+++ prebuild.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev libgtk-3-dev
