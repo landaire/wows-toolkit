5bc7f4e7b96ff1bdd3d41d782e59a5f5cdd4843a
Lander Brandt <landerbrandt@gmail.com>
2024-01-02 22:10:47 -0800

    use sh instead of bash for prebuild script

---
+++ prebuild.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev libgtk-3-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+fi
