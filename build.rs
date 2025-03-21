df7be366dfc40d7b3703e7a7b53104159e7c98b2
Lander Brandt <landergriffith@gmail.com>
2025-03-20 18:19:21 -0700

    fix: application icon is now embedded in binary/shows when pinned to taskbar

---
+++ build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+            let mut res = winresource::WindowsResource::new();
+            res.set_icon("assets/wows_toolkit.ico");
+            res.compile().unwrap();
+        }
+    }
+}
