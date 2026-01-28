96e582f62ce3c249eb9df4344b71854f99f3b577
Lander Brandt <landerbrandt@gmail.com>
2026-01-28 00:40:38 -0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "WoWs Toolkit devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+
+      rustToolchainToml = fromTOML (builtins.readFile ./rust-toolchain);
+      inherit (rustToolchainToml.toolchain) channel targets components;
+
+      rustToolchain = pkgs.rust-bin.stable.${channel}.default.override {
+        extensions = components;
+        inherit targets;
+      };
+    in
+      with pkgs; {
+        devShells.default = mkShell rec {
+          buildInputs = [
+            # Rust
+            rust-bin.stable.latest.default
+            rustToolchain
+
+            # misc. libraries
+            openssl
+            pkg-config
+
+            # GUI libs
+            libxkbcommon
+            libGL
+            fontconfig
+
+            # wayland libraries
+            wayland
+
+            # x11 libraries
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
